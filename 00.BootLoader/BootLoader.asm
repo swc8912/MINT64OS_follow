f2e9fa15b8ff3fb91540502f5e15d73e6a984359
swc8912 <swc8912@genikids.com>
2016-04-22 18:41:36 +0900

    ImageMaker 추가, 내가 컴파일 한 소스는 돌아가지 않아서 제공된 exe를 사용함, 32비트 시그윈으로 크로스컴파일 환경 만들었지만 빌드시에 ImageMaker를 쓰다가 lseek fail나서 일단 놔둠

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -6,7 +6,7 @@ SECTION .text	; text 섹션(세그먼트)을 정의
 jmp 0x07C0:START	; CS세그먼트 레지스터에 0x07C0을 복사하면서 START 레이블로 이동
 
 ; OS 관련된 환경 설정 값
-TOTALSECTORCOUNT:	dw 1	; 부트 로더를 제외한 OS 이미지의 크기, 최대 1152 섹터(0x90000byte)까지 가능
+TOTALSECTORCOUNT:	dw 0x02		; 부트 로더를 제외한 OS 이미지의 크기, 최대 1152 섹터(0x90000byte)까지 가능
 
 ; 코드 영역
 START:
