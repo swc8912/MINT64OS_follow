b23a91f1a15a293c1c7e53234b36eeeaf3c59a28
swc8912 <swc8912@gmail.com>
2017-01-12 00:50:15 +0900

    이미지메이커 소스 다시 원상복귀 그게 맞는 수정판임, 이미지메이커 제공된걸로 써야 제대로 돌아감, Page.h 추가

---
+++ 01.Kernel32/Source/Page.h
@@ -0,0 +1,45 @@
+#ifndef __PAGE_H__
+#define __PAGE_H__
+
+#include "Types.h"
+
+// 매크로
+#define  PAGE_FALGS_P		0x00000001  // Present
+#define  PAGE_FALGS_RW		0x00000002  // Read/Write
+#define  PAGE_FALGS_US		0x00000004  // User/Supervisor(플래그 설정 시 유저 레벨)
+#define  PAGE_FALGS_PWT		0x00000008  // Page Level Write-through
+#define  PAGE_FALGS_PCD		0x00000010  // Page Level Cache Disable
+#define  PAGE_FALGS_A		0x00000020  // Accessed
+#define  PAGE_FALGS_D		0x00000040  // Dirty
+#define  PAGE_FALGS_PS		0x00000080  // Page Size
+#define  PAGE_FALGS_G		0x00000100  // Global
+#define  PAGE_FALGS_PAT		0x00001000  // Page Attribute Table Index
+#define  PAGE_FALGS_EXB		0x80000000  // Execute Disable
+#define  PAGE_FALGS_DEFAULT	(PAGE_FLAGS_P | PAGE_FLAGS_RW)
+#define  PAGE_TABLESIZE		0x1000
+#define  PAGE_MAXENTRYCOUNT	512
+#define  PAGE_DEFAULTSIZE	0x200000
+
+// 구조체
+#pragma pack(push, 1)
+
+// 페이지 엔트리에 대한 자료구조
+typedef struct kPageTableEntryStruct
+{
+	// PML4T와 PDPTE의 경우
+	// 1 비트 P, RW, US, PWT, PCD, A 3 비트 Reserved, 3 비트 Avail,
+	// 20 비트 Base Address
+	// PDE의 경우
+	// 1 비트 P, RW, US, PWT, PCD, A, D, PS, G, 3 비트 Avail, 1 비트 PAT, 8 비트 Reserved,
+	// 11 비트 Base Address
+	DWORD dwAttributeAndLowerBaseAddress;
+	// 8 비트 Upper BaseAddress, 12 비트 Reserved, 11 비트 Avail, 1 비트 EXB
+	DWORD dwUpperBaseAddressAndEXB;
+} PML4TENTRY, PDPTENTRY, PDENTRY, PTENTRY;
+#pragma pack(pop)
+
+void kInitializePageTables(void);
+void KsetPageEntryData(PTENTRY* pstEntry, DWORD dwUpperBaseAddress,
+		DWORD dwLowerBaseAddress, DWORD dwLowerFlags, DWORD dwUpperFlags);
+
+#endif
